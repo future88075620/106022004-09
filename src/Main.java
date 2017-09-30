b537ebde8325a0496e08d383d799af9af5f2e60a
future88075620 <future870914@gmail.com>
2017-09-30 14:25:20 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	double n;
+    n=in.nextDouble();
+    System.out.printf("請輸入的數字為 : %.1f\n",n*1.6);
+    }
+}
